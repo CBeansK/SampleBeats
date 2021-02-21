f9d172774cf9f1b7cdd79e28031596bfe7343e37
CBeansK <ben.l.keen@gmail.com>
2021-02-20 17:47:44 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // display choices:
+        /*
+           1. Search for song
+                - ask for song title
+                - execute query
+                - display results
+           2. Insert new song
+                - ask for title
+                - ask for length
+                - ask for album
+                - if album already exists, use artist from album
+                    - else, ask for artist
+                - execute insert query
+                - if successful, display song data
+                    - else display error
+           3. Add sample to song
+                - pick song to add sample to
+                - pick song to add as sample
+                - TODO: flesh this out more
+           4. Delete song
+                - ask for song title
+                - get song
+                - display results and confirm deletion
+                - delete song, display success or failure
+
+
+           -------- Special Queries --------
+           5. Get all songs longer than a certain length
+                - ask for length
+                - execute query
+                - display results
+           6. Get samples from songs by a certain artist
+                - ask for artist
+                - execute query
+                - display results
+         */
+    }
+}
